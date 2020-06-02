45305c1da79b54a590de9c9feaee3b5b7a8d19e9
Anand Mohan <noobandy1364@gmail.com>
2020-06-02 23:01:33 +0530

    AWS S3 Storage

--- src/main/java/in/anandm/report/config/ReportConfig.java
+++ src/main/java/in/anandm/report/config/ReportConfig.java
@@ -4,30 +4,49 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import in.anandm.report.services.ReportDataService;
 import in.anandm.report.services.ReportStorageService;
-import in.anandm.report.services.FSReportStorageService;
+import in.anandm.report.services.S3ReportStorageService;
 
 @Configuration
 public class ReportConfig {
-	
+
 	@Value("${report.filePath}")
 	private String filePath;
+
+	@Value("${report.s3.fileName}")
+	private String s3FilePath;
+
+	@Value("${report.s3.bucketName}")
+	private String bucketName;
+
+	@Value("${report.s3.region}")
+	private String region;
 
 	@Bean
 	public ObjectMapper getObjectMapper() {
 		return new ObjectMapper();
 	}
-	
+
 	@Bean
 	public ReportDataService getReportDataService() {
 		return new ReportDataService();
 	}
-	
+
 	@Bean
 	public ReportStorageService getReportStorageService() {
-		return new FSReportStorageService(filePath, getObjectMapper());
+		return new S3ReportStorageService(getS3Client(), getObjectMapper(), bucketName, s3FilePath);
+	}
+
+	@Bean
+	public AmazonS3 getS3Client() {
+
+		return AmazonS3ClientBuilder.standard().withRegion(region).withCredentials(new ProfileCredentialsProvider())
+				.build();
 	}
 }
---
+++ src/main/java/in/anandm/report/services/S3ReportStorageService.java
@@ -0,0 +1,43 @@
+package in.anandm.report.services;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import in.anandm.report.model.ReportData;
+
+public class S3ReportStorageService implements ReportStorageService {
+
+	private AmazonS3 s3;
+	private ObjectMapper objectMapper;
+	private String bucketName;
+	private String fileName;
+	
+
+
+	public S3ReportStorageService(AmazonS3 s3, ObjectMapper objectMapper, String bucketName, String fileName) {
+		super();
+		this.s3 = s3;
+		this.objectMapper = objectMapper;
+		this.bucketName = bucketName;
+		this.fileName = fileName;
+	}
+
+
+
+	@Override
+	public void writeReportData(ReportData data) throws StorageException {
+		try {
+			s3.putObject(bucketName, fileName, objectMapper.writeValueAsString(data));
+		} catch (AmazonServiceException e) {
+			throw new StorageException(e);
+		} catch (SdkClientException e) {
+			throw new StorageException(e);
+		} catch (JsonProcessingException e) {
+			throw new StorageException(e);
+		}
+	}
+
+}
